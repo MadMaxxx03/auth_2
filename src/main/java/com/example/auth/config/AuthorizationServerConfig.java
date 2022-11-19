154490e0b0152d6320a64870b1fd23aacb821561
MadMaxxx03 <babenko.md@ya.ru>
2022-11-19 20:13:30 +0300

    2) oauth2 don't work

--- src/main/java/com/example/auth/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/auth/config/AuthorizationServerConfig.java
@@ -1,13 +1,7 @@
-package com.example.auth.configs;
-
+package com.example.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
@@ -15,7 +9,6 @@ import org.springframework.security.oauth2.server.authorization.client.InMemoryR
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
-import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.UUID;
 
@@ -23,29 +16,20 @@ import java.util.UUID;
 public class AuthorizationServerConfig {
 
     @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception {
-        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-        return http.formLogin(Customizer.withDefaults()).build();
-    }
-
-    @Bean
     public RegisteredClientRepository registeredClientRepository() {
         RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
-          .clientId("articles-client")
-          .clientSecret("{noop}secret")
+          .clientId("application-client")
+          .clientSecret("${app.jwtSecret}")
           .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
           .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
           .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-          .redirectUri("http://127.0.0.1:8080/login/oauth2/code/articles-client-oidc")
-          .redirectUri("http://127.0.0.1:8080/authorized")
+          .redirectUri("http://localhost:8090")
           .scope(OidcScopes.OPENID)
-          .scope("articles.read")
+          .scope("application.read")
           .build();
 
         return new InMemoryRegisteredClientRepository(registeredClient);
     }
-
 
     @Bean
     public ProviderSettings providerSettings() {
